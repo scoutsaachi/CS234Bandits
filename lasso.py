9c66362c14642ba88740bacbc0024f6808869043
Peter Lu <6peterlu@gmail.com>
2019-03-13 20:14:15 -0700

    lasso fix in progress

--- lasso.py
+++ lasso.py
@@ -1,38 +1,39 @@
 import numpy as np
-import cvxpy as cp
+
 from utils import history_index
+
+
 # based on http://web.stanford.edu/~bayati/papers/lassoBandit.pdf
+# more based on https://www0.gsb.columbia.edu/faculty/azeevi/PAPERS/2-arm-SSY-final.pdf
 class LassoBandit:
     def __init__(self):
-        self.forced_sample_schedule = {}  # index -> action to be forced, zero indexed
-        self.forced_sample_schedule_inv = {} # action -> list of indices
+        self.forced_sample_schedule = {
+        }  # index -> action to be forced, zero indexed
+        self.forced_sample_schedule_inv = {}  # action -> list of indices
         self.h = 5
-        self.initl2 = 0.05 # these can also be 0 maybe
-        self.l1 = 0.05
-        self.l2 = self.initl2
+        # self.initl2 = 0.05  # these can also be 0 maybe
+        # self.l1 = 0.05
+        # self.l2 = self.initl2
         self.context_size = 8
         self.num_actions = 3
 
         # self sampling params
-        self.ss_K = self.num_actions # num actions
-        self.ss_q = 1 # sampling hyperparam
+        self.ss_K = self.num_actions  # num actions
+        self.ss_q = 1  # sampling hyperparam
         self.load_forced_sample_schedule()
 
-    # honestly this is so sus
-    def beta_hat(self, X, y, l):  # l = lambda
-        n = len(X)
-        beta = cp.Variable(self.context_size)
-        objective = cp.Minimize(cp.sum_squares(y - X * beta) / n + l * cp.norm(beta, 1))
-        prob = cp.Problem(objective)
-        result = prob.solve()
-        return beta.value
+    def beta_hat(self, X, y):
+        XTX_inv = np.linalg.inv(X.T @ X)
+        return XTX_inv @ X @ y / len(X)
 
     # populate forced sample schedule
     def load_forced_sample_schedule(self):
         for action in range(self.ss_K):
-            for n in range(8): # yes magic #s
+            for n in range(8):  # yes magic #s
                 for j in range(1, self.ss_q + 1):
-                    sample_idx = (2 ** n - 1) * self.ss_K * self.ss_q + self.ss_q * action + j - 1
+                    sample_idx = (
+                        2**n -
+                        1) * self.ss_K * self.ss_q + self.ss_q * action + j - 1
                     self.forced_sample_schedule[sample_idx] = action
                     if action not in self.forced_sample_schedule_inv:
                         self.forced_sample_schedule_inv[action] = []
@@ -41,16 +42,21 @@ class LassoBandit:
     def predict(self, context, history):
         t = len(history)
         if t in self.forced_sample_schedule:
-            return self.forced_sample_schedule[len(history)] # TODO: possible off by one error
+            return self.forced_sample_schedule[len(history)]
 
-        # we're just going to eliminate the worst action rather than do the ones within a threshold
+        # MODIFICATION: we're just going to eliminate the worst action rather than do the ones within a threshold
         worst_action = None
         worst_action_score = None
         for action in range(self.num_actions):
-            forced_schedule = [x for x in self.forced_sample_schedule_inv[action] if x < t]
-            forced_samples_features = history_index(history, 0, t_arr=forced_schedule) # multiindexing probably only works in numpy
-            forced_samples_targets = history_index(history, 2, t_arr=forced_schedule)
-            beta = self.beta_hat(forced_samples_features, forced_samples_targets, self.l1)
+            forced_schedule = [
+                x for x in self.forced_sample_schedule_inv[action] if x < t
+            ]
+            forced_samples_features = history_index(
+                history, 0, t_arr=forced_schedule, add_one=True)
+            forced_samples_targets = history_index(
+                history, 2, t_arr=forced_schedule)
+            beta = self.beta_hat(forced_samples_features,
+                                 forced_samples_targets)
             action_score = context.T @ beta
             if worst_action_score == None:
                 worst_action_score = action_score
@@ -65,7 +71,8 @@ class LassoBandit:
         for action in range(self.num_actions):
             if action == worst_action:
                 continue
-            beta = self.beta_hat(history_index(history, 0), history_index(history, 2), self.l2) # this doesn't work IRL but this is the idea
+            beta = self.beta_hat(
+                history_index(history, 0), history_index(history, 2))
             action_score = context.T @ beta
             if best_action_score == None:
                 best_action = action
@@ -73,7 +80,8 @@ class LassoBandit:
             elif action_score > best_action_score:
                 best_action = action
                 best_action_score = action_score
-        self.l2 = np.sqrt((np.log(t) + np.log(self.context_size)) / t) * self.initl2
+        # self.l2 = np.sqrt(
+        #     (np.log(t) + np.log(self.context_size)) / t) * self.initl2
         print("best action: %d" % best_action)
         return best_action
 
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-import csv
 import argparse
+import csv
 from collections import defaultdict
+
 import numpy as np
+
 
 def read_data_file(filename, skip_header=True):
     skiprows = 0
@@ -12,23 +14,28 @@ def read_data_file(filename, skip_header=True):
     data = data[:, 1:-1]
     return data, labels
 
+
 def normalize(data):
     mean = np.mean(data, axis=1, keepdims=True)
     stdev = np.std(data, axis=1, keepdims=True)
-    return (data - mean)/stdev
+    return (data - mean) / stdev
+
 
 def maxmin_normalize(data):
     maxv = np.max(data, axis=1, keepdims=True)
     minv = np.min(data, axis=1, keepdims=True)
-    return (data - minv)/(maxv-minv)
+    return (data - minv) / (maxv - minv)
+
 
 def get_argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("datafile", help="name of the data file")
     parser.add_argument("bandit", help="name of bandit to run")
     parser.add_argument("--alpha", type=float, default=-1)
-    parser.add_argument("--process", type=str, default="none") # ["none", "maxmin", "norm"]
+    parser.add_argument(
+        "--process", type=str, default="none")  # ["none", "maxmin", "norm"]
     return parser
+
 
 def bucketize_action(dose):
     if dose < 21:
@@ -38,8 +45,12 @@ def bucketize_action(dose):
     else:
         return 2
 
-def history_index(history, idx, t_arr=[]):
+
+def history_index(history, idx, t_arr=[], add_one=False):
     if len(t_arr) > 0:
-        return np.array([history[t][idx] for t in t_arr])
-    else:
-        return np.array([row[idx] for row in history]) # returns all timesteps if no t_arr given
+        contexts = np.squeeze(np.array([history[t][idx] for t in t_arr]))
+        if add_one:
+            contexts = np.hstack((contexts, np.ones((len(t_arr), 1))))
+        return contexts
+    else:  # returns all timesteps if no t_arr given
+        return np.array([row[idx] for row in history])
