808638a9d6f6d041926e8904187e9f8c6182105a
ruechy <wanglucy@stanford.edu>
2019-03-20 20:43:05 -0700

    everyone now

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # array=( constant clinical linear thompson thompson2 lasso hyper randhyper random )
-array=( hyper randhyper random )
+#array=( hyper randhyper random )
+array=( hyper )
 
 for i in "${array[@]}"
 do
     echo $i
     mkdir -p results/$i
-	python3 main.py data/clean.csv $i results/$i/runone_$i.pkl
+	python3 main.py data/clean.csv $i paper_results/bernoulli/$i/runone_$i.pkl
     echo "running 10"
-    python3 main.py data/clean.csv $i results/$i/runten_$i.pkl --runten
+    python3 main.py data/clean.csv $i paper_results/bernoulli/$i/runten_$i.pkl --runten
 done 
